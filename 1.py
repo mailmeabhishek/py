6035985c20f10983b9a6a5c364d246f516686dec
Rana Abhishek <40243810+mailmeabhishek@users.noreply.github.com>
2026-06-28 22:11:02 +0530

    Secon Commit

--- 1.py
+++ 1.py
@@ -1,4 +1,10 @@
-msg = "Hello World"
-print(msg)
-new_msg = msg.replace('World','Universe')
-print(new_msg)
+# msg = "Hello World"
+# print(msg)
+# new_msg = msg.replace('World','Universe')
+# print(new_msg)
+
+greeting = 'Hello'
+name= 'John'
+
+message = '{}, {} . Welcome!'.format(greeting, name)
+print(message)
