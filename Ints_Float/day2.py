07589dd1b0a343418b888b9970e824f3b7491691
Rana Abhishek <40243810+mailmeabhishek@users.noreply.github.com>
2026-06-29 14:24:14 +0530

    Day 2 completed

--- Ints_Float/day2.py
+++ Ints_Float/day2.py
@@ -1,19 +1,44 @@
+print(".....Day2.....")
 # Arithmetic Operators:
 # Addition:       3 + 2
+print(3+2)
 # Subtraction:    3 - 2
+print(3-2)
 # Multiplication: 3 * 2
+print(3*2)
 # Division:       3 / 2
+print(3/2)
 # Floor Division: 3 // 2
+print(3//2)
 # Exponent:       3 ** 2
+print(3**2)
 # Modulus:        3 % 2
+print(3%2)
 
+print(round(3.75,1))
 
 # Comparisons:
+num_1= 3
+num_2 =2
 # Equal:            3 == 2
+print((num_1==num_2))
 # Not Equal:        3 != 2
+print(num_1!=num_2)
 # Greater Than:     3 > 2
+print(num_1>num_2)
 # Less Than:        3 < 2
+print(num_1<num_2)
 # Greater or Equal: 3 >= 2
+print(num_1>=num_2)
 # Less or Equal:    3 <= 2
-print(".....Day2.....")
+print(num_1<=num_2)
+
+#Casting in Python
+num_1 ='100'
+num_2 = '200'
+
+num_1 = int(num_1)
+num_2 = int(num_2)
+
+print(num_1 +num_2)
 
