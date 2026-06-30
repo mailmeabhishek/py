b879dad2d9e18e0167071a885b3de2c6002eb47b
Rana Abhishek <40243810+mailmeabhishek@users.noreply.github.com>
2026-06-30 16:17:35 +0530

    Conditonals and Booleans

---
+++ Conditionals_Booleans/day6.py
@@ -0,0 +1,36 @@
+# ..........Conditionals and Boolean
+
+# Comparisons:
+# Equal:            ==
+# Not Equal:        !=
+# Greater Than:     >
+# Less Than:        <
+# Greater or Equal: >=
+# Less or Equal:    <=
+# Object Identity:  is
+
+
+# print("Conditionals and Booleans")
+#
+# language = 'Java'
+#
+# if language == 'Python':
+#     print("Language is Python")
+# elif language == 'Java':
+#     print("Language is Java")
+# else:
+#     print("Language is not Python")
+
+# False Values:
+# False
+# None
+# Zero of any numeric type
+# Any empty sequence. For example, '', (), [].
+# Any empty mapping. For example, {}.
+
+condition = 'Test'
+
+if condition:
+    print("Evaluated to True")
+else:
+    print("Evaluated to False")
