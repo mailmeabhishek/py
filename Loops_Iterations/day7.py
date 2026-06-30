f458c53a102755605f9a3d5a7a476f97855d36c0
Rana Abhishek <40243810+mailmeabhishek@users.noreply.github.com>
2026-06-30 21:22:15 +0530

    Day 7 - Loops and Iterations

---
+++ Loops_Iterations/day7.py
@@ -0,0 +1,23 @@
+# nums = [1, 2, 3, 4, 5]
+#
+# for num in nums:
+#     if num == 3:
+#         print('Found the number')
+#         # break
+#         continue
+#     print(num)
+
+# for i in range(3, 20):
+#     print(i)
+
+
+#
+# while x < 10:
+#     print(x)
+#     x += 1
+x = 0
+while True:
+    if x == 5:
+        break
+    print(x)
+    x += 1
